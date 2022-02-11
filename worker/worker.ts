930c134a00f4dc3952990d79e6b3e88d6e199aaa
Denali <ddemots@capsule.com>
2022-02-11 00:31:30 -0500

    serve static assets

--- worker/worker.ts
+++ worker/worker.ts
@@ -1,12 +1,28 @@
 import { createRequestHandler } from "@remix-run/server-runtime";
+import type { ServerPlatform } from "@remix-run/server-runtime";
+import { handleAsset } from "@remix-run/cloudflare-workers";
 
 import * as build from "../build";
 
-const requestHandler = createRequestHandler(build, {});
+const platform: ServerPlatform = {};
+const requestHandler = createRequestHandler(build, platform);
 
-export async function fetch(request, env) {
+export async function fetch(request, env, ctx) {
   try {
-    return requestHandler(request, env);
+    const event = {
+      request,
+      waitUntil(promise) {
+        return ctx.waitUntil(promise);
+      },
+      passThroughOnException() {},
+    };
+    const assetOptions = {
+      ASSET_NAMESPACE: env.__STATIC_CONTENT,
+      ASSET_MANIFEST: env.__STATIC_CONTENT_MANIFEST,
+    };
+    let response = await handleAsset(event, build, assetOptions);
+    if (!response) response = await requestHandler(request, env);
+    return response;
   } catch (e: any) {
     return new Response(e.message || e.toString(), {
       status: 500,
