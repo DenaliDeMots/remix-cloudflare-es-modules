02ae8d9fd06daf55a10ad9a3a98cb98111f254a6
Denali <ddemots@capsule.com>
2022-02-11 02:59:47 -0500

    use build script for production

--- esbuild.js
+++ esbuild.js
@@ -1,13 +1,15 @@
 const { copy } = require("esbuild-plugin-copy");
 const { build } = require("esbuild");
+
 build({
   entryPoints: ["./worker/worker.ts"],
   bundle: true,
   sourcemap: true,
   format: "cjs",
   outfile: "./dist/worker.js",
+  minify: process.env.NODE_ENV === "production",
   define: {
-    "process.env.NODE_ENV": '"development"',
+    "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`,
   },
   plugins: [
     copy({
